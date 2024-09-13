e92dba338d66bc295c74b71314d6fbc96900ac41
rupi <varinderjit17@gmail.com>
2024-09-14 11:34:18 +1200

    Initial commit

---
+++ src/test/java/com/rupicodes/thymeleaf/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupicodes.thymeleaf.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
